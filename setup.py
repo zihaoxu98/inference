6941e389a879b3640c0294bffa88ac1a86ce9364
Zihao Xu <zihao.xu@columbia.edu>
2023-06-14 14:20:12 +0800

    rename

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name='aptinf',
+    name='inference',
     version='0.0.0',
-    description='Appletree inference',
-    author='Appletree contributors, the XENON collaboration',
+    description='A toolkit for convenient likelihood definition and statistical inference',
+    author='Zihao Xu',
     setup_requires=['pytest-runner'],
     python_requires='>=3.8',
     packages=setuptools.find_packages(),
-    url="https://github.com/XENONnT/aptinf",
+    url="https://github.com/xzh19980906/inference",
     zip_safe=False,
 )
